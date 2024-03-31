22cb203728e2f24da0648b9019d3fd0f0597f73f
Akshay <akshaykurian528@gmail.com>
2024-04-01 00:06:15 +0530

    Fifth Commit

--- app.js
+++ app.js
@@ -3,10 +3,16 @@ const bodyParser = require('body-parser');
 const swaggerUi = require('swagger-ui-express');
 const swaggerJSDoc = require('swagger-jsdoc');
 const pricingRoutes = require('./routes/pricing');
+const cors = require('cors');
+
 
 const app = express();
 
+app.use(cors());
+
 app.use(bodyParser.json());
+
+
 
 // Swagger setup
 const swaggerOptions = {
@@ -28,7 +34,7 @@ const swaggerOptions = {
 };
 
 const swaggerSpec = swaggerJSDoc(swaggerOptions);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 // Routes
 app.use('/api/pricing', pricingRoutes);
@@ -37,9 +43,4 @@ const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-});
-
-
-app.get('/', (req, res) => {
-    res.redirect('/api-docs'); 
 });
