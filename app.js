3ed30259ce3c1b84d2484649aae890f4362b3594
Akshay <akshaykurian528@gmail.com>
2024-03-31 14:47:49 +0530

    Second Commit

--- app.js
+++ app.js
@@ -38,3 +38,13 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the Food Delivery API');
+});
+
+
+app.get('/', (req, res) => {
+    res.redirect('/api-docs'); // Redirect to Swagger documentation
+});
